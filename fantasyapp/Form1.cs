4e459412d224d5075607130ea707c5880e37450c
Steven Havko <stevenhavko@gmail.com>
2015-09-11 15:39:59 -0400

    end of day

--- fantasyapp/Form1.cs
+++ fantasyapp/Form1.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace fantasyapp
 {
+     
     public partial class MyFantasyApp : Form
     {
+        
+       
         public MyFantasyApp()
         {
             InitializeComponent();
+            qb.Text = Reader.Read();
         }
+
+       
 
         private void updateLineup_Click(object sender, EventArgs e)
         {
@@ -23,37 +30,21 @@ namespace fantasyapp
             confirmUpdate.Visible = true;
         }
 
-    
+
 
         private void confirmUpdate_Click(object sender, EventArgs e)
         {
             qbUpdate.Visible = false;
             confirmUpdate.Visible = false;
-            qb.Text = qbUpdate.Text;
-        }
-
-        public class Player
-        {
-            public string name;
+            //qb.Text = qbUpdate.Text;
+            
+            //Writer.Write(qbUpdate.Text);
             
         }
 
-        public void ReadXML()
-        {
-            var p = new Player { name = "Matt Ryan" };
-            var writer = new System.Xml.Serialization.XmlSerializer(typeof(Player));
-            var wfile = new System.IO.StreamWriter(@"C:\temp\SerializationOverview.xml");
-            writer.Serialize(wfile, p);
-            wfile.Close();
-
-            System.Xml.Serialization.XmlSerializer reader =
-              new System.Xml.Serialization.XmlSerializer(typeof(Player));
-            System.IO.StreamReader file = new System.IO.StreamReader(
-                @"c:\temp\SerializationOverview.xml");
-            Player overview = (Player)reader.Deserialize(file);
-            file.Close();
-
-            Console.WriteLine(overview.name);
+       
         }
-    }
+   
+
 }
+
---
+++ fantasyapp/Reader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace fantasyapp
+{
+    public class Reader
+    {
+        public static string Read()
+        {
+            string result = "Test 2";
+            XmlTextReader reader = new XmlTextReader ("Player.xml");
+
+            while (reader.Read())
+            {
+                result = reader.GetAttribute(0);
+            }
+            return result;
+        }
+    }
+}
+
